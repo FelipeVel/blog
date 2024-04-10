dcc75b6b5872ad7fc27631291b22f9cb297ab501
FelipeVel <fvelandia695@gmail.com>
2024-04-10 18:05:47 -0500

    Corrección de redireccionamiento en entorno desplegado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className='Header'>
-            <a href='/'>
-                <h1>Blogs Dummy API</h1>
-            </a>
+            <Link to={`/`}><h1>Blogs Dummy API</h1></Link>
         </div>
     )
 }
