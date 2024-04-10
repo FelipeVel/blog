2fe6146be3e76ea9ba5d5a0ddd4c58c6cc3ab374
FelipeVel <fvelandia695@gmail.com>
2024-04-10 17:49:37 -0500

    Arreglos de despliegue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Post from "./pages/Post";
